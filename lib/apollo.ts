cdc8a13302f94c45651f752a395ae58332efe2e4
Yago Lopez <yago.lopez@gmail.com>
2020-12-14 19:21:22 +0100

    Delete lib folder

--- lib/apollo.ts
+++
@@ -1,9 +0,0 @@
-import { withApollo } from 'next-apollo'
-import ApolloClient, { InMemoryCache } from 'apollo-boost'
-
-const apolloClient = new ApolloClient({
-  uri: "https://tmdb-graphql.com/",
-  cache: new InMemoryCache(),
-});
-
-export default withApollo(apolloClient)
