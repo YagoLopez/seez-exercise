97fdb17ed11761277f9f81379a0a8d432ff837e6
Yago Lopez <yago.lopez@gmail.com>
2020-12-14 19:49:30 +0100

    Add styles to drawer menu

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -11,6 +11,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import styles from './Layout.module.css'
 import { CONST } from '../../constants'
+import { Icon } from '@rmwc/icon'
 
 export default function Layout({ children }) {
   const [openDrawer, setOpenDrawer] = useState(false)
@@ -29,11 +30,13 @@ export default function Layout({ children }) {
         <DrawerContent>
           <List>
             <ListItem onClick={onClickRandomJoke}>
+              <Icon icon="search" className={styles.icon_color} />
               <Link href="/">
                 <a className={styles.menu_item}>Search</a>
               </Link>
             </ListItem>
             <ListItem onClick={onClickRandomJoke}>
+              <Icon icon="exit_to_app" className={styles.icon_color} />
               <Link href="/random/[]">
                 <a className={styles.menu_item}>Get Random Jokes</a>
               </Link>
