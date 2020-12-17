7f191bbba208486b6dbb7bbd00be2d903417c1a7
Yago Lopez <yago.lopez@gmail.com>
2020-12-17 23:43:03 +0100

    💥 Final refactors

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -18,7 +18,27 @@ const PageHead = ({
 }: PageHeadProps) => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <title>{title}</title>
+    <title>{title || ''}</title>
+    <meta
+      name="description"
+      content={description || CONST.PAGE_HEAD_DEFAULT_DESCRIPTION}
+    />
+    <meta
+      name="keywords"
+      content={keywords || CONST.PAGE_HEAD_DEFAULT_KEYWORDS}
+    />
+    <meta property="og:url" content={url || CONST.PAGE_HEAD_OG_URL} />
+    <meta property="og:title" content={title || ''} />
+    <meta
+      property="og:description"
+      content={description || CONST.PAGE_HEAD_DEFAULT_DESCRIPTION}
+    />
+    <meta name="twitter:site" content={url || CONST.PAGE_HEAD_OG_URL} />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:image" content={ogImage || CONST.PAGE_HEAD_OG_IMAGE} />
+    <meta property="og:image" content={ogImage || CONST.PAGE_HEAD_OG_IMAGE} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
   </NextHead>
 )
 
