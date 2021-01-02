b48cb2d36aaaeae8b253eca42cd4bbe773662ac2
Yago Lopez <yago.lopez@gmail.com>
2021-01-03 00:17:00 +0100

    Simplify error handling

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,19 +1,16 @@
-import { useRouter } from 'next/router'
 import css from '../public/styles/global.module.css'
 import { CONST } from '../constants'
+import Link from 'next/link'
 
 export const NoResults = ({ message }) => {
-  const router = useRouter()
-  const onGoBack = () => router.push('/')
-
   return (
     <div className={css.centerHorVer}>
-      <p data-cy="no-results-msg">{message ?? CONST.NO_RESULTS}</p>
-      <div>
-        <a href="#" onClick={onGoBack}>
-          ⇦ Go Back
-        </a>
-      </div>
+      <p className={css.textAlignCenter} data-cy="no-results-msg">
+        {message ?? CONST.NO_RESULTS}
+      </p>
+      <Link href="/" shallow={true}>
+        <a>⇦ Go Back</a>
+      </Link>
     </div>
   )
 }
--- services/jokes.repository.ts
+++ services/jokes.repository.ts
@@ -1,4 +1,4 @@
-import * as ErrorService from './errors.service'
+import JokeError from '../models/JokeError'
 
 /**
  * Chuck Norris Jokes Repository Class
@@ -10,12 +10,13 @@ export default class JokesRepository {
    * Utiliy function to fetch data from endpoint
    * @param endpoint url
    */
-  static getData = async (endpoint: string): Promise<object[]> => {
+  static getData = async (endpoint: string): Promise<object[] | JokeError> => {
     try {
       const response = await fetch(endpoint)
       return await response.json()
     } catch (exception) {
-      return ErrorService.parseObj(exception)
+      console.log(exception)
+      return { message: exception.message, error: exception.message }
     }
   }
 }
